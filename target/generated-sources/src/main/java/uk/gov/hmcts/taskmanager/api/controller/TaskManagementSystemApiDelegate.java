a3e4ebd3fe63044f111376c49c77143844f4c30e
AnonDeveloper <AnonDeveloper@github.co.uk>
2025-06-18 11:56:44 +0100

    refactor(yaml): open api
    task schema added to yaml file and api updated to return a task.
    
    Signed-off-by: AnonDeveloper <AnonDeveloper@github.co.uk>

---
+++ target/generated-sources/src/main/java/uk/gov/hmcts/taskmanager/api/controller/TaskManagementSystemApiDelegate.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.taskmanager.api.controller;
+
+import uk.gov.hmcts.taskmanager.domain.Task;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A delegate to be called by the {@link TaskManagementSystemApiController}}.
+ * Implement this interface with a {@link org.springframework.stereotype.Service} annotated class.
+ */
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-06-18T11:55:09.564508900+01:00[Europe/London]")
+public interface TaskManagementSystemApiDelegate {
+
+    /**
+     * @see TaskManagementSystemApi#getTask
+     */
+    ResponseEntity<Task> getTask( String  id);
+
+}
---
+++ target/generated-sources/src/main/java/uk/gov/hmcts/taskmanager/domain/Task.java
@@ -0,0 +1,181 @@
+package uk.gov.hmcts.taskmanager.domain;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Date;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
+/**
+ * Task
+ */
+@Validated
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-06-18T11:55:09.564508900+01:00[Europe/London]")
+
+
+public class Task   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("title")
+  private String title = null;
+
+  @JsonProperty("taskDescription")
+  private String taskDescription = null;
+
+  @JsonProperty("status")
+  private String status = null;
+
+  @JsonProperty("dueDate")
+  private Date dueDate = null;
+
+  public Task id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Id of the task to be updated.
+   * @return id
+   **/
+  @Schema(example = "10", description = "Id of the task to be updated.")
+      @NotNull
+
+    public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Task title(String title) {
+    this.title = title;
+    return this;
+  }
+
+  /**
+   * Get title
+   * @return title
+   **/
+  @Schema(description = "")
+      @NotNull
+
+    public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Task taskDescription(String taskDescription) {
+    this.taskDescription = taskDescription;
+    return this;
+  }
+
+  /**
+   * Get taskDescription
+   * @return taskDescription
+   **/
+  @Schema(description = "")
+  
+    public String getTaskDescription() {
+    return taskDescription;
+  }
+
+  public void setTaskDescription(String taskDescription) {
+    this.taskDescription = taskDescription;
+  }
+
+  public Task status(String status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * Get status
+   * @return status
+   **/
+  @Schema(description = "")
+      @NotNull
+
+    public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Task dueDate(Date dueDate) {
+    this.dueDate = dueDate;
+    return this;
+  }
+
+  /**
+   * Get dueDate
+   * @return dueDate
+   **/
+  @Schema(description = "")
+      @NotNull
+
+    @Valid
+    public Date getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(Date dueDate) {
+    this.dueDate = dueDate;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Task task = (Task) o;
+    return Objects.equals(this.id, task.id) &&
+        Objects.equals(this.title, task.title) &&
+        Objects.equals(this.taskDescription, task.taskDescription) &&
+        Objects.equals(this.status, task.status) &&
+        Objects.equals(this.dueDate, task.dueDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, title, taskDescription, status, dueDate);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Task {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    title: ").append(toIndentedString(title)).append("\n");
+    sb.append("    taskDescription: ").append(toIndentedString(taskDescription)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("    dueDate: ").append(toIndentedString(dueDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
